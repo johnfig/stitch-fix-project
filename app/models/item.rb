c025fc57a5de1170931f481e8f5ac4862e06c160
John Figueiredo <john@johnfigueiredo.com>
2015-05-08 21:29:45 -0700

    Update clearance! to calculate wholesale price
    - The wholesale price will now be accounted for type 'pants' and 'dress'
    that have a minimum of $5

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,21 @@ class Item < ActiveRecord::Base
   scope :sellable, -> { where(status: 'sellable') }
 
   def clearance!
-    update_attributes!(status: 'clearanced', 
-                       price_sold: style.wholesale_price * CLEARANCE_PRICE_PERCENTAGE)
+    update_attributes!(status: 'clearanced',
+                       price_sold: calculate_wholesale_price)
   end
 
+  private
+
+  def calculate_wholesale_price
+    wholesale_clearance_price < 5 && pants_or_dress? ? 5 : wholesale_clearance_price
+  end
+
+  def wholesale_clearance_price
+    style.wholesale_price * CLEARANCE_PRICE_PERCENTAGE
+  end
+
+  def pants_or_dress?
+    style.type == 'pants' || style.type == 'dress'
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,21 +1,51 @@
 require 'rails_helper'
 
 describe Item do
-  describe "#perform_clearance!" do
-
+  describe "#clearance!" do
     let(:wholesale_price) { 100 }
-    let(:item) { FactoryGirl.create(:item, style: FactoryGirl.create(:style, wholesale_price: wholesale_price)) }
+    let(:type)  { 'pants' }
+    let(:style) { FactoryGirl.create :style, wholesale_price: wholesale_price, type: type }
+    let(:item)  { FactoryGirl.create(:item, style: style) }
+
     before do
       item.clearance!
       item.reload
     end
 
-    it "should mark the item status as clearanced" do
-      expect(item.status).to eq("clearanced")
+    it 'should mark the item status as clearanced' do
+      expect(item.status).to eq('clearanced')
     end
 
-    it "should set the price_sold as 75% of the wholesale_price" do
-      expect(item.price_sold).to eq(BigDecimal.new(wholesale_price) * BigDecimal.new("0.75"))
+    context 'item is pants or a dress' do
+      context 'pants at 75% wholesale are less than $5' do
+        let(:wholesale_price) { 6 }
+        let(:type)  { 'pants' }
+
+        it 'returns a minimum of $5' do
+          expect(item.price_sold).to eq(BigDecimal.new('5'))
+        end
+      end
+
+      context 'dresses at 75% wholesale are less than $5' do
+        let(:wholesale_price) { 6 }
+        let(:type) { 'dress' }
+
+        it 'returns a minimum of $5' do
+          expect(item.price_sold).to eq(BigDecimal.new('5'))
+        end
+      end
+
+      context 'pants/dress at 75% wholesale are more than $5' do
+        it 'returns 75% of wholesale value' do
+          expect(item.price_sold).to eq(BigDecimal.new(wholesale_price) * BigDecimal.new("0.75"))
+        end
+      end
+    end
+
+    context 'item is not pants or a dress' do
+      it "should set the price_sold as 75% of the wholesale_price" do
+        expect(item.price_sold).to eq(BigDecimal.new(wholesale_price) * BigDecimal.new("0.75"))
+      end
     end
   end
 end

3f0388131c2916756b5156bfba135a9f70b73acf
John Figueiredo <john@johnfigueiredo.com>
2015-05-09 07:33:10 -0700

    Cleanup code to be cleaner, still needs refactor

--- app/controllers/clearance_batches_controller.rb
+++ app/controllers/clearance_batches_controller.rb
@@ -8,17 +8,20 @@ class ClearanceBatchesController < ApplicationController
     clearancing_status = ClearancingService.new.process_file(params[:csv_batch_file].tempfile)
     clearance_batch    = clearancing_status.clearance_batch
     alert_messages     = []
+
     if clearance_batch.persisted?
       flash[:notice]  = "#{clearance_batch.items.count} items clearanced in batch #{clearance_batch.id}"
     else
       alert_messages << "No new clearance batch was added"
     end
+
     if clearancing_status.errors.any?
       alert_messages << "#{clearancing_status.errors.count} item ids raised errors and were not clearanced"
       clearancing_status.errors.each {|error| alert_messages << error }
     end
+
     flash[:alert] = alert_messages.join("<br/>") if alert_messages.any?
     redirect_to action: :index
   end
-
 end
+
--- app/services/clearancing_service.rb
+++ app/services/clearancing_service.rb
@@ -1,12 +1,14 @@
 require 'csv'
 require 'ostruct'
-class ClearancingService
 
+class ClearancingService
   def process_file(uploaded_file)
-    clearancing_status      = create_clearancing_status
-    CSV.foreach(uploaded_file, headers: false) do |row|  
+    clearancing_status = create_clearancing_status
+
+    CSV.foreach(uploaded_file, headers: false) do |row|
       potential_item_id = row[0].to_i
       clearancing_error = what_is_the_clearancing_error?(potential_item_id)
+
       if clearancing_error
         clearancing_status.errors << clearancing_error
       else
@@ -14,36 +16,35 @@ class ClearancingService
       end
     end
 
-    clearance_items!(clearancing_status) 
+    clearance_items!(clearancing_status)
   end
 
-private
+  private
 
   def clearance_items!(clearancing_status)
-    if clearancing_status.item_ids_to_clearance.any? 
+    if clearancing_status.item_ids_to_clearance.any?
       clearancing_status.clearance_batch.save!
+
       clearancing_status.item_ids_to_clearance.each do |item_id|
         item = Item.find(item_id)
         item.clearance!
         clearancing_status.clearance_batch.items << item
       end
     end
+
     clearancing_status
   end
 
   def what_is_the_clearancing_error?(potential_item_id)
     if potential_item_id.blank? || potential_item_id == 0 || !potential_item_id.is_a?(Integer)
-      return "Item id #{potential_item_id} is not valid"      
-    end
-    if Item.where(id: potential_item_id).none?
-      return "Item id #{potential_item_id} could not be found"      
-    end
-    if Item.sellable.where(id: potential_item_id).none?
+      return "Item id #{potential_item_id} is not valid"
+    elsif Item.where(id: potential_item_id).none?
+      return "Item id #{potential_item_id} could not be found"
+    elsif Item.sellable.where(id: potential_item_id).none?
       return "Item id #{potential_item_id} could not be clearanced"
+    else
+      return nil
     end
-
-    return nil
-    
   end
 
   def create_clearancing_status
@@ -52,5 +53,4 @@ private
       item_ids_to_clearance: [],
       errors: [])
   end
-
 end

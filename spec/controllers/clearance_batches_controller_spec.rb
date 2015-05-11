0301898c5675192f5345e836f966078d9ee51050
John Figueiredo <john@johnfigueiredo.com>
2015-05-11 12:36:07 -0700

    Backfill ClearanceBatchController#create with csv fixture

--- spec/controllers/clearance_batches_controller_spec.rb
+++ spec/controllers/clearance_batches_controller_spec.rb
@@ -27,4 +27,28 @@ describe  ClearanceBatchesController do
       expect(assigns(:clearance_batch)).to eq clearance_batch
     end
   end
+
+  describe '#create' do
+    subject { get :create, csv_batch_file: fixture_file_upload('spec/fixtures/example_csv.csv', 'text/csv') }
+
+    context 'file upload' do
+      context 'clearance batch is persisted' do
+        let!(:item) { FactoryGirl.create :item, id: 488, status: 'sellable' }
+
+        it 'returns a flash notice with the item count in batch' do
+          subject
+          notice = "1 items clearanced in batch 1"
+          expect(flash[:notice]).to eq notice
+        end
+      end
+
+      context 'clearance batch is not persisted' do
+        it 'has a flash alert with errors about non persisted batch and not found items' do
+          subject
+          expect(flash[:alert]).to eq "No new clearance batch was added<br/>1 item ids raised errors and were not clearanced<br/>Item id 488 could not be found"
+        end
+      end
+    end
+  end
 end
+
